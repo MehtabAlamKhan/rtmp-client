430eadf82c6ef3c873b52af0e90005fa8f905edc
MehtabAlamKhan <kmehtabalam19@gmail.com>
2024-10-25 19:14:38 +0530

    changes

--- dist/client.js
+++ dist/client.js
@@ -9,18 +9,33 @@ const node_crypto_1 = __importDefault(require("node:crypto"));
 const node_net_1 = __importDefault(require("node:net"));
 /* 2048 is the prime number size (modulus size)
    it will automatically create predefined base(generator) and modulus for us */
-const dh = node_crypto_1.default.createDiffieHellman(2048);
+const dh = node_crypto_1.default.createDiffieHellman(800);
 /* this will generate both public and private
    keys but will return public key only */
 const clientPublicKey = dh.generateKeys();
 const clientPrivateKey = dh.getPrivateKey(); // we need to call the generateKeys first to get this
+const clientVersion = Buffer.from([3, 0, 0, 0]);
 const socket = node_net_1.default.createConnection({ host: "127.0.0.1", port: 1935 }, () => {
+    socket.write(clientVersion);
     socket.write(clientPublicKey.toString("base64"));
 });
+let sharedSecretKey = "";
 socket.on("data", (data) => {
-    const serverPublicKey = Buffer.from(data.toString("base64"), "base64");
-    //compute shared secret key (this is the final key)
-    const sharedSecretKey = dh.computeSecret(serverPublicKey);
+    if (!sharedSecretKey.length) {
+        const serverPublicKey = Buffer.from(data.toString("base64"), "base64");
+        //compute shared secret key (this is the final key)
+        const sharedSecretKey = dh.computeSecret(serverPublicKey);
+        const serverHmac = data.subarray(-32);
+        const hmac = node_crypto_1.default.createHmac("sha256", sharedSecretKey);
+        const expectedHmac = hmac.update(clientPublicKey).digest();
+        if (!expectedHmac.equals(serverHmac)) {
+            console.log("INVALID HMAC");
+            socket.destroy();
+            return;
+        }
+        const clientHmac = node_crypto_1.default.createHmac("sha256", sharedSecretKey).update(serverPublicKey).digest();
+        socket.write(clientHmac);
+    }
 });
 socket.on("close", () => {
     console.log("SERVER DISCONNECTED");
--- src/client.ts
+++ src/client.ts
@@ -6,23 +6,41 @@ import net from "node:net";
 
 /* 2048 is the prime number size (modulus size)
    it will automatically create predefined base(generator) and modulus for us */
-const dh = crypto.createDiffieHellman(2048);
+const dh = crypto.createDiffieHellman(800);
 
 /* this will generate both public and private
    keys but will return public key only */
 const clientPublicKey = dh.generateKeys();
 
 const clientPrivateKey = dh.getPrivateKey(); // we need to call the generateKeys first to get this
+const clientVersion = Buffer.from([3, 0, 0, 0]);
 
 const socket = net.createConnection({ host: "127.0.0.1", port: 1935 }, () => {
+  socket.write(clientVersion);
   socket.write(clientPublicKey.toString("base64"));
 });
 
-socket.on("data", (data) => {
-  const serverPublicKey = Buffer.from(data.toString("base64"), "base64");
+let sharedSecretKey = "";
 
-  //compute shared secret key (this is the final key)
-  const sharedSecretKey = dh.computeSecret(serverPublicKey);
+socket.on("data", (data) => {
+  if (!sharedSecretKey.length) {
+    const serverPublicKey = Buffer.from(data.toString("base64"), "base64");
+
+    //compute shared secret key (this is the final key)
+    const sharedSecretKey = dh.computeSecret(serverPublicKey);
+    const serverHmac = data.subarray(-32);
+    const hmac = crypto.createHmac("sha256", sharedSecretKey);
+    const expectedHmac = hmac.update(clientPublicKey).digest();
+
+    if (!expectedHmac.equals(serverHmac)) {
+      console.log("INVALID HMAC");
+      socket.destroy();
+      return;
+    }
+
+    const clientHmac = crypto.createHmac("sha256", sharedSecretKey).update(serverPublicKey).digest();
+    socket.write(clientHmac);
+  }
 });
 
 socket.on("close", () => {
